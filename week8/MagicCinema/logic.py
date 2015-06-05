e0ca45f9d5696af1e398547014188af0509a6620
Vladimir Petkov <vlado@itgbg.com>
2015-06-05 14:11:19 +0300

    fixing logic.py

--- week8/MagicCinema/logic.py
+++ week8/MagicCinema/logic.py
@@ -33,13 +33,19 @@ def show_movies():
         FROM Movies
         ORDER BY rating DESC
     """)
+
+    movie_ids = set()
+
     for row in result.fetchall():
         print("movie_id: {}".format(row[0]), "movie_name: {}".format(row[1]))
+        movie_ids.add(row[0])
+
+    return movie_ids
 
 
 # we have to handle the case where the name is not in reservation table
-    cursor.execute("""
 def cancel_reservation(name):
+    cursor.execute("""
         DELETE FROM Reservations
         WHERE username = \"{}\"
     """.format(name))
@@ -58,33 +64,75 @@ def show_movie_projections(movie_id, date=None):
     else:
         result = cursor.execute("""
             SELECT proj_id, time, type
-            FROM projections
+            FROM Projections
             WHERE movie_id = {} AND date = \'{}\'
             ORDER BY date ASC
         """.format(movie_id, date))
+
     for row in result.fetchall():
-        print(row)
+
+        print("projection_id: {}, date: {}, time: {}, type: {}".format(row[0],
+            row[1], row[2], row[3]))
 
 
 def choose_projection(projection_id):
     pass
 
 
+class GiveUp(Exception):
+    pass
+
+
+def input_or_give_up(argument):
+    var = input(argument)
+    if var == "give_up":
+        raise GiveUp
+    else:
+        return var
+
+
+def enter_username():
+    while True:
+        username = input_or_give_up("Please, enter your name!>")
+        if len(username) < 2:
+            continue
+        else:
+            return username
+
+
+def choose_movie():
+    movies_by_ids = show_movies()
+
+    while True:
+        chosen_movie_id = input_or_give_up("Choose movie by its 'movie_id'>")
+        try:
+            chosen_movie_id = int(chosen_movie_id)
+            if chosen_movie_id not in movies_by_ids:
+                continue
+            else:
+                return chosen_movie_id
+
+        except:
+            continue
+
+
 # tazi f-iq ne raboti, no nadejdata umira posledna ;)
 def make_reservation():
-    while True:
-        hacker = input("Choose name>")
-        number_of_tickets = input("Choose number of tickets>")
-        show_movies()
-        # sled kato pokaje projekciite trqbva otnovo da se vklu4i > i togava da me pita koq proj iskam
-        chosen_movie_id = input("Choose movie by its 'movie_id'>")
+
+    try:
+        hacker = enter_username()
+        number_of_tickets = input_or_give_up("Please, enter the number of tickets U want!>")
+        chosen_movie_id = choose_movie()
         show_movie_projections(chosen_movie_id, date=None)
-        choosen_projection = input("Choose projection by its projection_id>")
-        choose_projection(choosen_projection)  # tova 6te go pazim v bazata
-        aviable_seats()
-        choose_seat()  # tova 6te go pazim v bazata
-        print("This is your reservation:\n {}\n Date and Time: {}, type\n {}".format(movie_id, date, time, type, seats))
-        conn.commit()
+        chosen_projection = input_or_give_up("Choose projection by its 'projection_id'>")
+        # choose_projection(chosen_projection)  # tova 6te go pazim v bazata
+        # aviable_seats()
+        # choose_seat()  # tova 6te go pazim v bazata
+        # # print("This is your reservation:\n {}\n Date and Time: {}, type\n {}".format(movie_id, date, time, type, seats))
+        # conn.commit()
+    except GiveUp:
+        print("U gave up the reservation pracess.")
+        return
 
 
 def close_connection():
